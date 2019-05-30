55feb17753a4a70d3e64e4fced12bd4d78685364
Jonathan Araujo Duarte <jonathan.aradu@gmail.com>
2019-05-31 01:27:47 +0200

    Removed custom values for web dist

--- infra/main.tf
+++ infra/main.tf
@@ -94,9 +94,6 @@ resource "aws_cloudfront_distribution" "s3_website_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
   restrictions {
